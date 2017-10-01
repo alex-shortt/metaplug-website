54bbdd344c6f9bc17fe6d7a74a5cf610969ee922
Alex Shortt <Alex Shortt>
2017-09-30 18:00:12 -0700

    added everything except for modal, still need to catch breakpoints

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
-var currDays = 0;
-var currHours = 0;
-var currMins = 0;
-var currSecs = 0;
+var currDays = -1;
+var currHours = -1;
+var currMins = -1;
+var currSecs = -1;
 
 function initCircles() {
     var circleOptions = {
@@ -12,10 +12,11 @@ function initCircles() {
         foregroundColor: '#fff',
         noPercentageSign: 1,
         fontColor: "#fff",
-        percentageY: 100,
+        percentageY: 102,
         textColor: "#fff",
-        textStyle: "font-size: 8pt",
-        animation: 0
+        textStyle: "font-size: 7pt; font-family: Metropolis;",
+        animation: 0,
+        textAdditionalCss: "font-size: 35px; font-family: MetropolisBold;"
     };
 
     $("#circle-days").circliful(jQuery.extend({
@@ -34,54 +35,57 @@ function initCircles() {
         text: "Seconds"
     }, circleOptions));
 
-
-    var countDownDate = new Date("Oct 23, 2017 16:30:00").getTime();
-
-    var x = setInterval(function () {
-        var now = new Date().getTime();
-        var distance = countDownDate - now;
-
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        if (currDays != days) {
-            $('#circle-days').empty().removeData().circliful(jQuery.extend({
-                text: "Days",
-                percent: parseInt(days * 100 / 31),
-                replacePercentageByText: "" + days
-            }, circleOptions));
-            currDays = days;
-        }
-        if (currHours != hours) {
-            $('#circle-hours').empty().removeData().circliful(jQuery.extend({
-                text: "Hours",
-                percent: parseInt(hours * 100 / 60),
-                replacePercentageByText: "" + hours
-            }, circleOptions));
-            currHours = hours;
-        }
-        if (currMins != minutes) {
-            $('#circle-mins').empty().removeData().circliful(jQuery.extend({
-                text: "Mins",
-                percent: parseInt(minutes * 100 / 60),
-                replacePercentageByText: "" + minutes
-            }, circleOptions));
-            currMins = minutes;
-        }
-        if (currSecs != seconds) {
-            $('#circle-secs').empty().removeData().circliful(jQuery.extend({
-                text: "Seconds",
-                percent: parseInt(seconds * 100 / 60),
-                replacePercentageByText: "" + seconds
-            }, circleOptions));
-            currSecs = seconds;
-        }
-
-        if (distance < 0) {
-            clearInterval(x);
-            console.log("expired");
-        }
-    }, 1000);
+    updateTime(circleOptions);
+    var x = setInterval(updateTime, 1000, circleOptions);
 }
+
+
+function updateTime(circleOptions) {
+    var countDownDate = new Date("Oct 23, 2017 16:30:00").getTime();
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    if (currDays != days) {
+        $('#circle-days').empty().removeData().circliful(jQuery.extend({
+            text: days == 1 ? "Day" : "Days",
+            percent: parseInt(days * 100 / 31),
+            replacePercentageByText: "" + days
+        }, circleOptions));
+        currDays = days;
+    }
+    if (currHours != hours) {
+        $('#circle-hours').empty().removeData().circliful(jQuery.extend({
+            text: hours == 1 ? "Hour" : "Hours",
+            percent: parseInt(hours * 100 / 60),
+            replacePercentageByText: "" + hours
+        }, circleOptions));
+        currHours = hours;
+    }
+    if (currMins != minutes) {
+        $('#circle-mins').empty().removeData().circliful(jQuery.extend({
+            text: minutes == 1 ? "Minute" : "Minutes",
+            percent: parseInt(minutes * 100 / 60),
+            replacePercentageByText: "" + minutes
+        }, circleOptions));
+        currMins = minutes;
+    }
+    if (currSecs != seconds) {
+        $('#circle-secs').empty().removeData().circliful(jQuery.extend({
+            text: seconds == 1 ? "Second" : "Seconds",
+            percent: parseInt(seconds * 100 / 60),
+            replacePercentageByText: "" + seconds
+        }, circleOptions));
+        currSecs = seconds;
+    }
+
+    if (distance < 0) {
+        clearInterval(x);
+        console.log("expired");
+    }
+}
+
